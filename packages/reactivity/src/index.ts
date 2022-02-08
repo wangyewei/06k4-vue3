3e1d2643206b26adbb81947ac2a2c5203872a07f
WangYeWei <826036140@qq.com>
2022-02-08 19:34:40 +0800

    tsconfig

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -4,12 +4,12 @@
  * @WeChat: Studio06k4
  * @Motto: 求知若渴，虚心若愚
  * @Description: 
- * @LastEditTime: 2022-02-07 18:50:13
+ * @LastEditTime: 2022-02-08 19:24:56
  * @Version: 06k4 vue3
  * @FilePath: \06k4-vue3\packages\reactivity\src\index.ts
- * @Autor: YeWei Wang
  */
 
+import {Shared} from '@vue/shared'
 const Reactivity = {
 
 }
