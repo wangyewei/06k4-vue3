405cd53bcd63781cb88cb489eb0bedf629a6d43a
WangYeWei <826036140@qq.com>
2022-02-11 02:22:43 +0800

    baseHandlers

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -4,23 +4,49 @@
  * @WeChat: Studio06k4
  * @Motto: 求知若渴，虚心若愚
  * @Description: 实现new Proxy(target, handler)
- * @LastEditTime: 2022-02-10 20:09:42
+ * @LastEditTime: 2022-02-11 02:13:45
  * @Version: 06k4 vue3
  * @FilePath: \06k4-vue3\packages\reactivity\src\baseHandlers.ts
  */
 
-export const mutableHandlers = {
 
+// 仅读的属性set报错 是否是深度
+
+function createGetter(isReadonly = false, shallow = false) {
+  return function get() {
+
+  }
+}
+
+const get = createGetter()
+const shallowGet = createGetter(false, true)
+const readonlyGet = createGetter(true)
+const shallowReadonlyGet = createGetter(true, false)
+
+export const mutableHandlers: ProxyHandler<any> = {
+  get
 }
 
 export const shallowHandlers = {
-
+  get: shallowGet,
+  set(target, key) {
+    console.warn(`Set opreation on key ${String(key)} failed: target is readonly.`,
+      target
+    )
+    return target
+  }
 }
 
 export const readonlyHandlers = {
-
+  get: readonlyGet
 }
 
 export const shallowReadonlyHandlers = {
-
+  get: shallowReadonlyGet,
+  set(target, key) {
+    console.warn(`Set opreation on key ${String(key)} failed: target is readonly.`,
+      target
+    )
+    return target
+  }
 }
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -4,17 +4,14 @@
  * @WeChat: Studio06k4
  * @Motto: 求知若渴，虚心若愚
  * @Description: reactive api
- * @LastEditTime: 2022-02-10 20:09:01
+ * @LastEditTime: 2022-02-11 02:19:35
  * @Version: 06k4 vue3
  * @FilePath: \06k4-vue3\packages\reactivity\src\reactive.ts
  */
-
-
 import {
   isObject,
   toRawType
 } from "@vue/shared"
-// 是否仅读、柯里化
 
 import {
   mutableHandlers,
@@ -25,24 +22,24 @@ import {
 
 export const enum ReactiveFlags {
   SKIP = '__v_skip',
-    IS_REACTIVE = '__v_isReactive',
-    IS_READONLY = '__v_isReadObly',
-    IS_SHALLOW = '__v_isShallow',
-    RAW = '__v_raw'
+  IS_REACTIVE = '__v_isReactive',
+  IS_READONLY = '__v_isReadObly',
+  IS_SHALLOW = '__v_isShallow',
+  RAW = '__v_raw'
 }
 
 export interface Target {
-  [ReactiveFlags.SKIP] ? : boolean,
-  [ReactiveFlags.IS_REACTIVE] ? : boolean,
-  [ReactiveFlags.IS_READONLY] ? : boolean,
-  [ReactiveFlags.IS_SHALLOW] ? : boolean,
-  [ReactiveFlags.RAW] ? : boolean
+  [ReactiveFlags.SKIP]?: boolean,
+  [ReactiveFlags.IS_REACTIVE]?: boolean,
+  [ReactiveFlags.IS_READONLY]?: boolean,
+  [ReactiveFlags.IS_SHALLOW]?: boolean,
+  [ReactiveFlags.RAW]?: boolean
 }
 
 const enum TargetType {
   INVALID = 0,
-    COMMON = 1,
-    COLLECTION = 2
+  COMMON = 1,
+  COLLECTION = 2
 }
 
 // WeakSet 对象是一些对象值的集合, 并且其中的每个对象值都只能出现一次。在WeakSet的集合中是唯一的
@@ -64,29 +61,52 @@ function targetTypeMap(rawType: string) {
 function getTargetType(value: Target) {
   // 是否跳过或者是否能添加属性
   // isExtensible判断一个对象是否可以添加属性
-  return value[ReactiveFlags.SKIP] || !Object.isExtensible(value) ?
-    TargetType.INVALID :
-    targetTypeMap(toRawType(value))
+  return value[ReactiveFlags.SKIP] || !Object.isExtensible(value)
+    ? TargetType.INVALID
+    : targetTypeMap(toRawType(value))
 }
 
 export function reactive(target: object) {
   return createReactiveObject(target, false, mutableHandlers)
 }
 
-export function shallowReactive(target: object) {
-  return createReactiveObject(target, false, shallowHandlers)
+export declare const ShallowReactiveMarker: unique symbol
+
+export type ShallowReactive<T> = T & { [ShallowReactiveMarker]?: true }
+
+export function shallowReactive<T extends object>(
+  target: T
+): ShallowReactive<T> {
+  return createReactiveObject(
+    target,
+    false,
+    shallowHandlers
+  )
 }
 
 export function readonly(target: object) {
-  return createReactiveObject(target, true, readonlyHandlers)
+  return createReactiveObject(
+    target, 
+    true, 
+    readonlyHandlers)
 }
 
-export function shallowReadonly(target: object) {
-  return createReactiveObject(target, true, shallowReadonlyHandlers)
+/**
+ *  返回响应式对象根的副本
+ *  属性是只读的，不能递归转换或延展
+ *  返回properties
+ *  用于有状态组件创建props代理对象
+ */
+
+export function shallowReadonly<T extends object>(target: T): Readonly<T> {
+  return createReactiveObject(
+    target,
+    true,
+    shallowReadonlyHandlers)
 }
 
-// 会自动垃圾回收，不会造成内存泄露，存储的Key只能是对象
-export const reactiveMap = new WeakMap<Target, any> ()
+// 自动垃圾回收，不造成内存泄露，存储的Key只能是对象
+export const reactiveMap = new WeakMap<Target, any>()
 export const shallowReactiveMap = new WeakMap<Target, any>()
 export const readonlyMap = new WeakMap<Target, any>()
 export const shallowReadonlyMap = new WeakMap<Target, any>()
@@ -94,10 +114,9 @@ export const shallowReadonlyMap = new WeakMap<Target, any>()
 function createReactiveObject(
   target: Target,
   isReadonly: boolean,
-  baseHanderls: ProxyHandler < any > ,
-  collectionHandlers ? : ProxyHandler < any >
+  baseHanderls: ProxyHandler<any>,
+  collectionHandlers?: ProxyHandler<any>
 ) {
-  // 如果目标不是对象
   if (!isObject(target)) {
     return target
   }
@@ -106,9 +125,9 @@ function createReactiveObject(
 
   // 如果对象被代理就不需要再次被代理了
   const proxyMap = isReadonly ? readonlyMap : reactiveMap
-  
+
   const existingProxy = proxyMap.get(target)
-  if(existingProxy) {
+  if (existingProxy) {
     return existingProxy // 如果被代理就直接返回
   }
   const proxy = new Proxy(
