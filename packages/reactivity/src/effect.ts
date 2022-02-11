b9c45320f5d2e54cb763a1303db033df3a62207b
WangYeWei <826036140@qq.com>
2022-02-12 00:43:57 +0800

    fix up: effect dose not work

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -4,12 +4,15 @@
  * @WeChat: Studio06k4
  * @Motto: 求知若渴，虚心若愚
  * @Description: 依赖收集
- * @LastEditTime: 2022-02-12 00:17:03
+ * @LastEditTime: 2022-02-12 00:40:44
  * @Version: 06k4 vue3
  * @FilePath: \06k4-vue3\packages\reactivity\src\effect.ts
  */
 
+import { extend } from "@vue/shared"
+
 class ReactiveEffect<T = any> {
+  active = true
   constructor(
     public fn: () => T
   ) {
@@ -17,7 +20,13 @@ class ReactiveEffect<T = any> {
   }
   
   run() {
+    if(!this.active) {
+      return this.fn()
+    }
 
+    console.log('create effect')
+    console.log(this.fn)
+    return this.fn()
   }
 }
 
@@ -39,7 +48,12 @@ export function effect<T = any>(
   
   const _effect = new ReactiveEffect(fn)
 
+  if(options) {
+    extend(_effect, options)
+  }
+
   if(!options || !options.lazy) {
+    // console.log()
     _effect.run()
   }
 
--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -4,15 +4,18 @@
  * @WeChat: Studio06k4
  * @Motto: 求知若渴，虚心若愚
  * @Description: 导出响应式方法
- * @LastEditTime: 2022-02-11 23:35:57
+ * @LastEditTime: 2022-02-12 00:37:26
  * @Version: 06k4 vue3
  * @FilePath: \06k4-vue3\packages\reactivity\src\index.ts
  */
 // import { Shared } from "@vue/shared"
-debugger
 export {
   reactive,
   shallowReactive,
   readonly,
   shallowReadonly
-} from '../../reactivity/src/reactive'
+} from './reactive'
+
+export {
+  effect
+} from './effect'
